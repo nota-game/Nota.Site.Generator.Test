161de5a52bbaeeb8b516c745d6b7bcd92997f65a
Patrick Kranz <patrick-kranz@live.de>
2020-02-12 08:21:46 +0100

    init

---
+++ update.ps1
@@ -0,0 +1,27 @@
+if(!(Test-Path -Path '_gitView')){
+    New-Item -ItemType Directory '_gitView'
+    git clone 'remoteGit/content' '_gitView/content'
+    git clone 'remoteGit/schema' '_gitView/schema'
+    git clone 'remoteGit/target' '_gitView/website'
+}
+
+$currentLocation =  Get-Location;
+
+Set-Location '_gitView/content'
+git add .
+git commit -m '...'
+git push --tags
+Set-Location $currentLocation
+
+Set-Location '_gitView/schema'
+git add .
+git commit -m '...'
+git push --tags
+Set-Location $currentLocation
+
+Set-Location '_gitView/website'
+git add .
+git reset --hard
+git pull
+Set-Location $currentLocation
+
