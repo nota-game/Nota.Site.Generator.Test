765580b02ac7d84d93568945fa0ceab77f9b7d44
Patrick Kranz <patrick-kranz@live.de>
2020-02-14 00:40:22 +0100

    Push now working again

--- update.ps1
+++ update.ps1
@@ -10,12 +10,14 @@ $currentLocation =  Get-Location;
 Set-Location '_gitView/content'
 git add .
 git commit -m '...'
+git push
 git push --tags
 Set-Location $currentLocation
 
 Set-Location '_gitView/schema'
 git add .
 git commit -m '...'
+git push 
 git push --tags
 Set-Location $currentLocation
 
